f6dc516a7876dce1dfb427498fd579781083e360
Samyllesmarcione <101689015+Samyllesmarcione@users.noreply.github.com>
2022-05-15 16:22:25 -0300

    Add files via upload

---
+++ model/Usuario.java
@@ -0,0 +1,151 @@
+package com.blogpessoal.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy  = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message= "Esse campo é obrigatório!")
+	@Size(max=255)
+	private String nome;
+	
+	@NotBlank(message= "Esse campo é obrigatório!")
+	@Size(max=255)
+	private String usuario;
+	
+	@NotBlank(message= "Esse campo é obrigatório!")
+	@Size(max=255)
+	private String senha;
+	
+	@NotBlank(message= "Esse campo é obrigatório!")
+	@Size(max=255)
+	private String foto;
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+	private String tipo;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+
+	public long getId() {
+		return id;
+	}
+
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+
+	public String getFoto() {
+		return foto;
+	}
+
+
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+
+
+	public Usuario(long id, String nome,String usuario, String senha,String foto, String tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.tipo = tipo;
+	}
+
+
+
+	public Usuario() {
+		super();
+	}
+	
+	
+}  
+
+	
